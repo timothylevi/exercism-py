8c8a7bd7a0d7beca32a78af3cdf1ae669b5e1dd0
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-15 12:59:52 -0400

    Leap solution

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,12 @@
-def is_leap_year():
-    pass
+def is_leap_year(year):
+    def year_div_by(num):
+        return year % num == 0
+
+    if year_div_by(400):
+      return True
+    elif year_div_by(100):
+      return False
+    elif year_div_by(4):
+      return True
+
+    return False
