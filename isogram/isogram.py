187e8fc93c549f3e67ff243e47163980f688992d
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-15 14:05:57 -0400

    Solve isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,16 @@
-def is_isogram():
-    pass
+import re
+
+def is_isogram(word):
+    if len(word) == 0:
+        return True
+
+    stash = {}
+    word = re.sub('[\W_]+', '', word).lower()
+
+    for char in word:
+        if stash.get(char):
+            return False
+
+        stash[char] = True
+
+    return True
