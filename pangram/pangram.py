0b84ea95d2ffc09e0d39d7f89715705a577b7e21
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-16 18:16:12 -0400

    Implement isPangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,10 @@
-def is_pangram():
-    pass
+import re, string
+
+def is_pangram(sentence):
+    sentence = re.sub('[\d\W_]+', '', sentence).lower()
+    alphabet = string.ascii_lowercase
+
+    sentence_set = set(sentence)
+    alphabet_set = set(alphabet)
+
+    return len(sentence_set) == len(alphabet_set)
