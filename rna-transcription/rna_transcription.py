4a332fa51ad19db25e98a89f59f0dea3ee9140a9
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-16 22:05:40 -0400

    Naive implementation

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,14 @@
-def to_rna():
-    pass
+def to_rna(sequence):
+    complements = {
+        'G': 'C',
+        'C': 'G',
+        'T': 'A',
+        'A': 'U'
+    }
+
+    try:
+        sequence = [complements[nucleotide] for nucleotide in sequence]
+    except KeyError as e:
+        return ''
+
+    return ''.join(sequence)
