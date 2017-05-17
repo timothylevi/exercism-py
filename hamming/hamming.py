6aba112cd671af5dfd65859ffb416ab3e8549d07
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-17 15:21:57 -0400

    Implement hamming distance

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,12 @@
-def distance():
-    pass
+def distance(strand1, strand2):
+    """Return the number of differences between the two DNA strands."""
+    if len(strand1) != len(strand2):
+        raise ValueError
+
+    differences = 0
+
+    for index, char in enumerate(strand1):
+        if char != strand2[index]:
+            differences += 1
+
+    return differences
