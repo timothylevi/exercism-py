134b30d486284f3e40087fe4944fde9cf62a7c7a
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-17 18:14:23 -0400

    Implement `hey()`

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,18 @@
-def hey():
-    pass
+import re
+
+def hey(expression):
+    """Returns a different response based on the type of expression."""
+    expression = expression.strip()
+    expression_subbed = re.sub('[\d\W_]+', '', expression)
+
+    is_question = expression.endswith("?")
+    is_exclamation = expression == expression.upper() and expression_subbed
+
+    if not expression:
+        return "Fine. Be that way!"
+    elif is_exclamation:
+        return "Whoa, chill out!"
+    elif is_question:
+        return "Sure."
+
+    return "Whatever."
