0e4a2e5ce0fe2cb7e7f19e377b88b8db1712cc34
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-17 16:10:36 -0400

    Implement word-count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,17 @@
-def word_count():
-    pass
+import re
+from collections import defaultdict
+
+def word_count(phrase):
+    """
+    Replace spaces, non-alphanumeric, and underscore characters with tab
+    set case of phrase to lowercase and split using default pattern. Returns
+    a dictionary with words as keys and counts as values.
+    """
+
+    phrase = re.sub(r'[\s\W_]+', '\t', phrase).lower().split()
+    words = defaultdict(lambda: 0)
+
+    for word in phrase:
+        words[word] = words[word] + 1
+
+    return dict(words)
