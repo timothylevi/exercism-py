1e80d64a0e579d94e8b1e8f1bff3813101baf766
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-17 16:16:35 -0400

    Use Counter instead of re-writing functionality

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,17 +1,11 @@
 import re
-from collections import defaultdict
+from collections import Counter
 
 def word_count(phrase):
     """
     Replace spaces, non-alphanumeric, and underscore characters with tab
     set case of phrase to lowercase and split using default pattern. Returns
-    a dictionary with words as keys and counts as values.
+    a Counter with words as keys and counts as values.
     """
-
     phrase = re.sub(r'[\s\W_]+', '\t', phrase).lower().split()
-    words = defaultdict(lambda: 0)
-
-    for word in phrase:
-        words[word] = words[word] + 1
-
-    return dict(words)
+    return Counter(phrase)
