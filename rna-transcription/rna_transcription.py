3b4ece37b32141cea38c75c9b319ee7354b32dfc
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-16 22:06:10 -0400

    Write it  more like the JavaScript solution

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,14 +1,21 @@
-def to_rna(sequence):
-    complements = {
-        'G': 'C',
-        'C': 'G',
-        'T': 'A',
-        'A': 'U'
-    }
+COMPLEMENTS = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+}
 
+def get_rna_complement(nucleotide):
+    """Return the RNA complement to the DNA nucleotide."""
+    return COMPLEMENTS[nucleotide]
+
+def to_rna(sequence):
+    """"
+    If an input is invalid, return empty string. Otherwise, return a string
+    with each DNA nucleotide replaced with it's complementary RNA nucleotide.
+    """
     try:
-        sequence = [complements[nucleotide] for nucleotide in sequence]
+        sequence = [get_rna_complement(nucleotide) for nucleotide in sequence]
+        return ''.join(sequence)
     except KeyError as e:
         return ''
-
-    return ''.join(sequence)
