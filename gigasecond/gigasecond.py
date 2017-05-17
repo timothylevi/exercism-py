1a68cfabdc3546c2a8ae5b69bb000103406c9fbd
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-17 17:40:11 -0400

    Implement gigasecond and update test

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,2 +1,6 @@
-def add_gigasecond():
-    pass
+import datetime
+
+def add_gigasecond(past_date):
+    """Return passed date plus 10^9 seconds."""
+    gigasecond = datetime.timedelta(seconds=1000000000)
+    return past_date + gigasecond
--- gigasecond/gigasecond_test.py
+++ gigasecond/gigasecond_test.py
@@ -35,8 +35,8 @@ class GigasecondTest(unittest.TestCase):
 
     def test_yourself(self):
         # customize this to test your birthday and find your gigasecond date:
-        your_birthday = datetime(1970, 1, 1)
-        your_gigasecond = datetime(2001, 9, 9, 1, 46, 40)
+        your_birthday = datetime(1991, 3, 2)
+        your_gigasecond = datetime(2022, 11, 8, 1, 46, 40)
 
         self.assertEqual(add_gigasecond(your_birthday), your_gigasecond)
 
