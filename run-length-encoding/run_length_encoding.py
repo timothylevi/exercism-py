992b6316b892cbef74fb18337d0ab21411d0da23
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-17 20:13:39 -0400

    Implement run-length-encoding

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,34 @@
-def decode():
-    pass
+def decode(doc):
+    encoded = []
+    times = 0
+
+    for char in doc:
+        if char.isdigit():
+            times = times * 10 + int(char)
+        elif char.isalnum() or char.isspace():
+            for i in range(times if times else 1):
+                encoded.append(char)
+            times = 0
+
+    return ''.join(encoded)
 
 
-def encode():
-    pass
+def encode(doc):
+    decoded = []
+
+    count = 0
+    letter = ""
+
+    for char in doc:
+        if char == letter:
+            count += 1
+        else:
+            decoded.append(str(count) if count > 1 else '')
+            decoded.append(letter)
+            letter = char
+            count = 1
+
+    decoded.append(str(count) if count > 1 else '')
+    decoded.append(letter)
+
+    return ''.join(decoded)
