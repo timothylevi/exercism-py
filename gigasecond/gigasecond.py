48cc8abc3388b5d56c1bb5f9700d56fb4df156ee
Tim Campbell <timothylevicampbell@gmail.com>
2017-06-11 16:51:07 -0400

    Replace long number with exponential notation

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -2,5 +2,5 @@ import datetime
 
 def add_gigasecond(past_date):
     """Return passed date plus 10^9 seconds."""
-    gigasecond = datetime.timedelta(seconds=1000000000)
+    gigasecond = datetime.timedelta(seconds=1e9)
     return past_date + gigasecond
