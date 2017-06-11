2af4720beec4362e8080a0cf9a44b226f5e1f909
Tim Campbell <timothylevicampbell@gmail.com>
2017-06-11 16:47:34 -0400

    Update regex to reduce redundant checking

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -7,5 +7,5 @@ def word_count(phrase):
     set case of phrase to lowercase and split using default pattern. Returns
     a Counter with words as keys and counts as values.
     """
-    phrase = re.sub(r'[\s\W_]+', '\t', phrase).lower().split()
+    phrase = re.sub(r'[\W_]+', '\t', phrase).lower().split()
     return Counter(phrase)
